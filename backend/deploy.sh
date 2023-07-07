da47ba2b06ef2f6caa452bb210bd5d9a9cc123d6
Nguyen Ba Son <nguyenbason@windowslive.com>
2023-07-07 11:25:03 +0700

    first commit

---
+++ backend/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ev
+
+cd "$PWD"/Backend
+
+echo "Install Dependencies"
+npm ci
+
+echo "Set Max Garbage Heap of Node to 1024"
+export NODE_OPTIONS=--max-old-space-size=${MAX_OLD_SPACE_SIZE}
+
+echo "Deploy Application In Dev Stage"
+serverless deploy -v -s dev
